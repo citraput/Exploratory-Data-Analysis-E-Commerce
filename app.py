576b5346aed92bc341537893d920c8f902e0b37d
citraput <49798775+citraput@users.noreply.github.com>
2020-07-08 13:06:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, \
+    jsonify, request, send_from_directory, Flask 
+import requests
+import mysql.connector as mc
+
+app = Flask(__name__)
+
+@app.route('/')
+def beranda():
+    return render_template('home.html')
+
+# @app.route('/')
+# def beranda():
+#     return render_template('home.html')
+
+@app.errorhandler(404)
+def NotFound(error):
+    return render_template('error.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost', port=5000)
